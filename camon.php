d4fa9ca11164d7fc98b735a9bcf76cd387465484
anh210802 <77352818+anh210802@users.noreply.github.com>
2021-06-07 09:07:29 +0700

    Add files via upload

---
+++ camon.php
@@ -0,0 +1,36 @@
+﻿<html>
+<head>
+<title>TỚ ♥ CẬU - CHỈ VẬY THÔI</title>
+<link rel="shortcut icon" href="http://kênhit.vn/favicon.ico" type="image/png" />
+<link rel="stylesheet" href="css/reset.css" />
+    <link rel="stylesheet" href="css/style.css" />
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+	<script src="js/jquery.1.7.2.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="js/jquery-ui-1.8.23.custom.min.js"></script>
+	<script type="text/javascript" src="js/jquery.spritely-0.6.1.js"></script>
+	<script src="js/jquery.global.js" type="text/javascript"></script>
+<!--
+
+   __ __  //|          __     ____  ______      _   __  _  __
+  / //_/ |/||  ___    / /    /  _/ /_  __/     | | / / / |/ /
+ / ,<   / -_) / _ \  / _ \  _/ /    / /   _    | |/ / /    / 
+/_/|_|  \__/ /_//_/ /_//_/ /___/   /_/   (_)   |___/ /_/|_/  
+                                                             
+Trang tổng hợp, chia sẻ code hay
+url: http://kênhit.vn (Tên miền có dấu Tiếng Việt)
+-->
+</head>
+<body>
+<article id="cloud">
+<p><font color="#d64f1e">TỚ</font> <font color="#c41818">♥</font> <font color="#db3376">CẬU</font> - <font color="#fff">CHỈ VẬY THÔI</font></p>
+</article>
+<footer id="city"></footer>
+<div id="dapxe"><center><embed width="500" height="500"  wmode="transparent" menu="false" quality="high" type="application/x-shockwave-flash" src="images/dapxe.swf" pluginspage="http://www.macromedia.com/go/getflashplayer"/></center></div>
+<div id="duong"><p>&copy; <script type='text/javascript'>var creditsyear = new Date();document.write(creditsyear.getFullYear());</script><font color="#d64f1e"><b> Lâm Ngọc Khương - kênhit.vn</b></font> - Dành tặng cho <font color="#db3376"><b>Các bạn nhé</b></font></br></br>
+Những ngày qua ở bên cậu tớ cảm thấy</br>
+chẳng còn điều gì khiến tớ hạnh phúc hơn thế nữa</br>
+Tớ không dám nói là sẽ yêu cậu đến hết cuộc đời này</br>
+Nhưng tớ dám chắc một điều là: "Tớ Yêu Cậu" chỉ vậy thôi!</p></div>
+<iframe width="0" height="0" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/118319793&amp;color=ff6600&amp;auto_play=true&amp;show_artwork=true"></iframe>
+</body>
+</html>
